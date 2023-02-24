0e09f431da5fcc5fc730ac0abf716fb51bfb2f2d
Zashaaa <info@louisfares.nl>
2023-02-24 18:16:10 +0100

    Day 10, save progress

--- Day10/01_FunctionsWithOutput.py
+++ Day10/01_FunctionsWithOutput.py
@@ -1,9 +1,13 @@
 # Functions with outputs
 
 def format_name(first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "Names cannot be empty"
     first_name = first_name.title()
     last_name = last_name.title()
     return f"{first_name} {last_name}"
 
 
-print(format_name("MAX", "VERstaPPEN"))
+fn = input("What is your first name? ")
+ln = input("What is your last name? ")
+print(format_name(fn, ln))
