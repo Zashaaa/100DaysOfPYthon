ee818fde4fec3beaeb5c02483003fa1407e217c6
Zashaaa <info@louisfares.nl>
2023-02-24 22:51:09 +0100

    Day 10: Calculator project

--- Day10/Project_Calculator.py
+++ Day10/Project_Calculator.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 
 def add(n1, n2):
@@ -25,12 +24,25 @@ operations = {
     "/": devide
 }
 
-num1 = int(input("What's your first number?: "))
 
-for operation in operations:
-    print(operation)
+def calculator():
+    print(logo)
+    num1 = float(input("What's your first number?: "))
 
-desired_operation = input("Choose your desired operation from de symbols above: ")
-num2 = int(input("What's your second number?: "))
-answer = operations[desired_operation](num1, num2)
-print(f"{num1} {desired_operation} {num2} = {answer}")
+    for operation in operations:
+        print(operation)
+
+    continue_calculating = True
+    while continue_calculating:
+        desired_operation = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        answer = operations[desired_operation](num1, num2)
+        print(f"{num1} {desired_operation} {num2} = {answer}")
+        if input(f"Type 'y'to continue calculating with {answer}, or Type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            continue_calculating = False
+            calculator()
+
+
+calculator()
