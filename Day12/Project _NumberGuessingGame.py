8622e9f3aa212b6c8013a02cf0a9d0d585ce8742
Zashaaa <info@louisfares.nl>
2023-02-28 21:53:18 +0100

    Day 12: Guessing number. Working version

--- Day12/Project _NumberGuessingGame.py
+++ Day12/Project _NumberGuessingGame.py
@@ -10,7 +10,7 @@ logo = '''
 print(logo)
 print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
 number_to_guess = random.randint(1, 100)
-print(f"Psst, the number is {number_to_guess}")
+# print(f"Psst, the number is {number_to_guess}")
 
 right_input = False
 while not right_input:
@@ -25,3 +25,26 @@ while not right_input:
         print("Incorrect difficulty level , please try again....")
 
 print(f"You have {attempts} attempts remaining to guess the number.")
+
+
+def wrong_guess(number):
+    if number > number_to_guess:
+        print("Too high.\nGuess again")
+    else:
+        print("Too low.\nGuess again")
+
+
+guessed = False
+while not guessed and attempts > 0:
+    guessed_number = int(input("Make a guess: "))
+    if guessed_number == number_to_guess:
+        print(f"You got it! The answer was {number_to_guess}.")
+        guessed = True
+    else:
+        attempts -= 1
+        if attempts > 0:
+            wrong_guess(guessed_number)
+            print(f"You have {attempts} attempts remaining to guess the number.")
+        else:
+            print(f"You've lost... The correct answer was {number_to_guess}.")
+
