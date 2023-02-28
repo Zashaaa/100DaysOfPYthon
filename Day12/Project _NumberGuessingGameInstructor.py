58bcd23e31fbdb96c7db85f36f3624f70e83722a
Zashaaa <info@louisfares.nl>
2023-02-28 22:25:54 +0100

    Day 12: Guessing number. instructor version

---
+++ Day12/Project _NumberGuessingGameInstructor.py
@@ -0,0 +1,54 @@
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+
+logo = '''
+  / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | |__   ___ _ __ 
+ / /_\/ | | |/ _ \/ __/ __|   / /\/ '_ \ / _ \  /  \/ / | | | '_ ` _ \| '_ \ / _ \ '__|
+/ /_\| |_| |  __/\__ \__ \  / /  | | | |  __/ / /\  /| |_| | | | | | | |_) |  __/ |   
+\____/ \__,_|\___||___/___/  \/   |_| |_|\___| \_\ \/  \__,_|_| |_| |_|_.__/ \___|_|  
+'''
+
+
+def check_answer(guess, answer, turns):
+    """Checks answer against the guess. Returns to number of turns remaining"""
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer}.")
+
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"Psst, the number is {answer}")
+
+    turns = set_difficulty()
+    guess = 0
+    while guess != answer and turns > 0:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print(f"You've lost... The correct answer was {answer}.")
+            return
+        else:
+            print("Guess again")
+
+
+game()
