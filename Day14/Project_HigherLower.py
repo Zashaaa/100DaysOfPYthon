60b81358fb6ef0a3461f22aa9483c49d1c1583f4
Zashaaa <info@louisfares.nl>
2023-03-02 20:19:21 +0100

    Day 14, finished

--- Day14/Project_HigherLower.py
+++ Day14/Project_HigherLower.py
@@ -29,22 +29,23 @@ def check_answer(a, b, guess):
 
 
 def game():
-    compare_a = {}
-    compare_b = {}
+    print(logo)
+    score = 0
     correct = True
     while correct:
-        if "name" in compare_a:
-            compare_a = compare_b
-        else:
+        if score == 0:
             compare_a = get_item_to_compare([])  # First run
+        else:
+            compare_a = compare_b
         compare_b = get_item_to_compare(compare_a)
         print(f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}.")
-        print(compare_a["follower_count"])
         print(vs)
         print(f"Against B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}.")
-        print(compare_b["follower_count"])
         answer = input("Who has more followers? Type 'A' or 'B': ").lower()
         correct = check_answer(compare_a, compare_b, answer)
+        if correct:
+            score += 1
+    print(f"Game over! Your score is {score}")
 
 
 game()
